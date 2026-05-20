c9907fcff17992b6236e57c75b6156db2cc82ec4
heinricov abraham <heinricoov@gmail.com>
2026-05-20 17:46:29 +0700

    update 2

--- apps/web/components/sections/navbar.tsx
+++ apps/web/components/sections/navbar.tsx
@@ -43,7 +43,7 @@ export default function Navbar() {
                 </span>
               </Link>
               <div className="flex items-center gap-2">
-                <div className="flex items-center gap-2">
+                <div className="flex items-center gap-2 md:hidden">
                   <FaInstagram />
                   <FaXTwitter />
                   <FaSquareFacebook />
