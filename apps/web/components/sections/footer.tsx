13b25b2e96549b41ee648b4f7e52ead593ba1eb1
heinricov abraham <heinricoov@gmail.com>
2026-05-21 14:28:19 +0700

    update 6

--- apps/web/components/sections/footer.tsx
+++ apps/web/components/sections/footer.tsx
@@ -42,23 +42,31 @@ export default function Footer() {
       <div className="mx-auto max-w-7xl px-6">
         <div className="flex flex-wrap justify-between gap-12">
           <div className="order-last flex flex-col items-start gap-4 md:order-first">
-            <Link
-              href="/"
-              aria-label="home"
-              className="flex items-center gap-2"
-            >
-              <Image src={microvLogo} alt="Palapa" width={28} height={28} />
-              <span className="font-mono text-2xl font-medium tracking-tight uppercase">
-                Palapa
-              </span>
-            </Link>
+            <div className="flex flex-col items-start gap-2">
+              <Link
+                href="/"
+                aria-label="home"
+                className="flex items-center gap-2"
+              >
+                <Image src={microvLogo} alt="Palapa" width={28} height={28} />
+                <span className="font-mono text-2xl font-medium tracking-tight uppercase">
+                  Palapa
+                </span>
+              </Link>
+              <div className="mx-auto flex gap-7 md:mx-0 md:hidden">
+                <FaInstagram />
+                <FaXTwitter />
+                <FaSquareFacebook />
+                <FaLinkedin />
+              </div>
+            </div>
             <span className="block text-center text-sm text-muted-foreground">
               © {2026} Tailark Mist, All rights reserved
             </span>
           </div>
 
           <div className="flex flex-col items-end gap-6">
-            <div className="mx-auto flex gap-7 md:mx-0">
+            <div className="mx-auto hidden gap-7 md:mx-0 md:flex">
               <FaInstagram />
               <FaXTwitter />
               <FaSquareFacebook />
